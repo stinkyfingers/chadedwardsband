83f0bb69a3b6675aeb2a1fb7354b5d94aed6d6a3
johnshenk <jshenk@demandanlytics.io>
2022-11-09 16:56:49 -0600

    add www redirect to root

--- terraform/main.tf
+++ terraform/main.tf
@@ -126,8 +126,8 @@ resource "aws_route53_record" "www_record" {
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+    name                   = aws_route53_record.record.name
+    zone_id                = var.zone_id
     evaluate_target_health = false
   }
 }
