46f57456cdd69e515bec4edf819dab01f439ae8d
stinkyfingers <john_shenk@hotmail.com>
2022-12-03 00:37:18 -0600

    add sort

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Error from './Error';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown, faArrowUp, faArrowsV } from '@fortawesome/free-solid-svg-icons'
 import '../css/songlist.css';
 
 const SongList = ({
   songlist,
   err,
 }) => {
+    const [sort, setSort] = React.useState({ field: 'title' });
   const rows = () => {
     if (!songlist.length) return null;
-    return songlist.map((song) => (
+    return songlist
+        .sort((a, b) => {
+            if (a[sort.field] > b[sort.field]) {
+                return sort.asc ? 1 : -1
+            } else {
+                return sort.asc ? -1 : 1
+            }
+        })
+        .map((song) => (
       <tr key={song.Song}>
         <td>{song.Song}</td>
         <td>{song['Artist Version']}</td>
@@ -18,6 +29,19 @@ const SongList = ({
     ))
   };
   
+  const sortIcon = (field) => {
+      if (field !== sort.field) return faArrowsV;
+      return sort.asc ? faArrowUp : faArrowDown;
+  };
+  
+  const handleClick = (field) => {
+      if (field !== sort.field) {
+          setSort({ field, asc: true });
+          return;
+      }
+      setSort({ field, asc: !sort.asc });
+  };
+  
   if (!songlist) return null;
   return (
     <div className="songlist">
@@ -25,10 +49,18 @@ const SongList = ({
       <table>
         <thead>
           <tr>
-            <th>Title</th>
-            <th>Artist</th>
-            <th>Genre/Decade</th>
-            <th>Year</th>
+            <th>Title
+                <FontAwesomeIcon icon={sortIcon('Song')} onClick={() => handleClick('Song')}/>
+            </th>
+            <th>Artist
+                <FontAwesomeIcon icon={sortIcon('Artist Version')} onClick={() => handleClick('Artist Version')}/>
+            </th>
+            <th>Genre/Decade
+                <FontAwesomeIcon icon={sortIcon('Genre/Decade')} onClick={() => handleClick('Genre/Decade')}/>
+            </th>
+            <th>Year
+                <FontAwesomeIcon icon={sortIcon('Year')} onClick={() => handleClick('Year')}/>
+            </th>
           </tr>
         </thead>
         <tbody>
