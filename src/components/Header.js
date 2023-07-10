7d1b2b66f599a22be33f7985660970d9557acb6e
johnshenk <jshenk@demandanlytics.io>
2023-07-10 07:51:36 -0500

    make songs /songs

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Header = ({ err }) => {
       <Link className={`link${path === '/' && ' selected'}`} to="/">Home</Link>
       <Link className={`link${path === '/tour' && ' selected'}`} to="/tour">Tour</Link>
       <Link className={`link${path === '/about' && ' selected'}`} to="/about">About</Link>
-      <Link className={`link${path === '/request' && ' selected'}`} to="/request">Songs</Link>
+      <Link className={`link${(path === '/songs' || path === '/request') && ' selected'}`} to="/songs">Songs</Link>
       <Link className={`link${path === '/photos' && ' selected'}`} to="/photos">Photos</Link>
       <Link className={`link${path === '/video' && ' selected'}`} to="/video">Video</Link>
       <Link className={`link${path === '/tech' && ' selected'}`} to="/tech">Tech</Link>
