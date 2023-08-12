07b4ec485500512c8d594c3b38caaf6397bdfd6f
John Shenk <john_shenk@hotmail.com>
2023-08-12 18:25:47 -0500

    Add admin & photo control (#5)
    * photo admin wip
    
    * adds admin photos
    
    * cleanup
    
    * add date selector on photos
    
    ---------
    
    Co-authored-by: johnshenk <jshenk@demandanlytics.io>

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -2,52 +2,106 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import '../css/media.css';
 
-import AWS from 'aws-sdk';
+import { listPhotos } from '../Api';
 
-const accessKeyId = process.env.REACT_APP_AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
-const imageBucket = 'chadedwardsbandimages';
-const bucketUrl = `https://chadedwardsbandimages.s3.us-west-1.amazonaws.com/`;
-
-// randomize randomizes an array
-const randomize = (arr) => {
-  // swap swaps 2 array elements
-  const swap = (arr, i1, i2) => {
-    [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
-  };
-  
-  let index = arr.length - 1;
-  while (index > 0) {
-    const rand = Math.floor(Math.random() * index);
-    swap(arr, index, rand);
-    index--;
-  }
-  return arr;
-};
+const extractDate = (datetimeStr) => { 
+  const date = new Date(datetimeStr);
+  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+}
 
 const Photos = ({ setErr }) => {
   const [images, setImages] = React.useState([]);
+  const [filteredImages, setFilteredImages] = React.useState([]);
+  const [filter, setFilter] = React.useState({});
+  const [filterOptions, setFilterOptions] = React.useState({});
   
-  AWS.config.update({ region: 'us-west-1', secretAccessKey, accessKeyId });
+  const getCityState = (photoData) => {
+    return photoData.location.label.replace(photoData.location.name + ', ', '');
+  };
+
   React.useEffect(() => {
-    const credentials = new AWS.Credentials(accessKeyId, secretAccessKey);
-    const s3 = new AWS.S3({ apiVersion: '2006-03-01', credentials });
-    s3.listObjects({Bucket: imageBucket}, (err, data) => {
-      if (err) {
+    listPhotos(null)
+      .then((res) => {
+        const options = { category: new Set(), tags: new Set(), location: new Set(), date: new Set() };
+        setImages(res.map((photoData) => ({
+          ...photoData,
+            original: photoData.image,
+            thumbnail: photoData.thumbnail,
+            originalTitle: photoData.filename,
+          })))
+        res.forEach((photoData) => {
+          if (photoData.category) options.category.add(photoData.category);
+          if (photoData.tags?.length) photoData.tags.forEach((tag) => {
+            if (!tag) return;
+            options.tags.add(tag);
+          });
+          if (photoData.location?.label && photoData.location?.name) options.location.add(getCityState(photoData));
+          if (photoData.datetimeOriginal) options.date.add(extractDate(photoData.datetimeOriginal))
+        })
+        setFilterOptions(options)
+      })
+      .catch((err) => {
         console.warn(err)
-        return
-      }
-      const img = [];
-      data.Contents.forEach(datum => {
-        img.push({ original: `${bucketUrl}${datum.Key}`, thumbnail: `${bucketUrl}${datum.Key}`, originalTitle: datum.Key });
-      });      
-      setImages(randomize(img));
-    });
-  }, []);
+        setErr(err)
+      });
+  }, [setErr, setImages, filter]);
   
+  React.useEffect(() => {
+    setFilteredImages(images.filter((photoData) => {
+      return (
+        !filter.category || photoData.category === filter.category
+      ) && (
+        !filter.tags || (photoData.tags && photoData.tags.includes(filter.tags))
+      ) && (
+        !filter.location || getCityState(photoData) === filter.location
+      ) && (
+        !filter.date || extractDate(photoData.datetimeOriginal) === filter.date
+      )
+    }));
+  }, [filter, images])
+  
+  const handleChange = (e) => {
+    setFilter((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  };
 
   return <div className='media'>  
-    <ImageGallery items={images} />
+    <div className="controls">
+      <select className='photoSelector' value={filter.category} name="category" onChange={handleChange}>
+        <option key="select-category" value="">--category--</option>
+        {
+          filterOptions.category ? Array.from(filterOptions.category).map((opt) => (
+            <option key={opt} value={opt}>{opt}</option>
+          )) : null
+        }
+      </select>
+      <select className='photoSelector' value={filter.tags} name="tags" onChange={handleChange}>
+        <option key="select-tag" value="">--tag--</option>
+        {
+          filterOptions.tags ? Array.from(filterOptions.tags).map((opt) => (
+            <option key={opt} value={opt}>{opt}</option>
+          )) : null
+        }
+      </select>
+      <select className='photoSelector' value={filter.location} name="location" onChange={handleChange}>
+        <option key="select-location" value="">--location--</option>
+        {
+          filterOptions.location ? Array.from(filterOptions.location).map((opt) => (
+            <option key={opt} value={opt}>{opt}</option>
+          )) : null
+        }
+      </select>
+      <select className='photoSelector' value={filter.date} name="date" onChange={handleChange}>
+        <option key="select-date" value="">--date--</option>
+        {
+          filterOptions.date ? Array.from(filterOptions.date).sort((a, b) => (
+            a < b ? 1 : -1
+          )).map((opt) => (
+            <option key={opt} value={opt}>{opt}</option>
+          )) : null
+        }
+      </select>
+    </div>
+    <ImageGallery items={filteredImages} />
   </div>
 };
 
---
+++ src/components/admin/Photos.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { AdminContext } from '../../Context';
+import { listPhotos, updateMetadata, deletePhoto } from '../../Api';
+import PhotoMetadata from './PhotoMetadata';
+import { Navigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+const AdminPhotos = ({ setErr }) => {
+  const [admin] = React.useContext(AdminContext);
+  const [photos, setPhotos] = React.useState([]);
+  const [deleting, setDeleting] = React.useState(false);
+  const [metadata, setMetadata] = React.useState({});
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    if (!admin) {
+      return;
+    }
+    const fetchPhotos = async () => {
+      try {
+        const photoData = await listPhotos(admin.access_token);
+        if (photoData.error) {
+          setErr(photoData.error);
+          return;
+        }
+        setPhotos(photoData.sort((a, b) => a.filename > b.filename ? 1 : -1));
+        setMetadata(() => {
+          const data = {}
+          photoData.forEach((photo) => {
+            data[photo.filename] = photo
+          })
+          return data;
+        });
+      } catch (err) {
+        setErr(err);
+      }
+    }
+    fetchPhotos();
+   }, [admin]);
+
+  const handleMetadata = ({ id, category, tags }) => {
+    setMetadata((prev) => ({
+      ...prev,
+      [id]: {
+        ...prev[id],
+        category,
+        tags
+      }
+    }));
+  };
+
+  const handleSave = () => {
+    setLoading(true)
+    updateMetadata(admin.access_token, metadata).then((res) => {
+      setMetadata(res)
+    }).catch((err) => {
+      setErr(err)
+    }).finally(() => {
+      setLoading(false)
+    })
+  };
+  
+  const handleDelete = (photo) => {
+    setDeleting(true);
+    deletePhoto(admin.access_token, photo.filename).then((res) => {
+      setPhotos(photos.filter((p) => p.filename !== photo.filename))
+    }).catch((err) => {
+      setErr(err);
+    }).finally(() => {
+      setDeleting(false);
+    });
+  };
+  
+  if (!admin) {
+    return <Navigate to="/admin" />;
+  }
+	return (
+		<div className='adminPhotos'>
+      <div className='controls'>
+        <button onClick={handleSave} className='button'>Save</button>
+        { deleting && <div>Deleting...</div> }
+      </div>
+      <div className='photosSelectContainer'>
+      { !loading ? photos.map((photo) => (
+        <div key={photo.filename}>
+          <img src={photo.thumbnail} alt={photo.filename} />
+          <div><FontAwesomeIcon className='deleteIcon' icon={faTrash} onClick={() => handleDelete(photo)} /></div>
+          <PhotoMetadata id={photo.filename} metadata={metadata[photo.filename]} handleMetadata={handleMetadata} />
+        </div>
+        )) : <div>Loading...</div>
+      }
+      </div>
+    </div>
+	);
+};
+
+export default AdminPhotos;
