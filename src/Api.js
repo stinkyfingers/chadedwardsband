ea422c525d6df95bed9090bb4de9c7f32ee41235
johnshenk <jshenk@demandanlytics.io>
2024-10-29 16:01:39 -0500

    update chatgpt version

--- src/Api.js
+++ src/Api.js
@@ -119,7 +119,7 @@ export const chatGptCompletion = async({ messages }) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-4o-mini',
       messages: messages,
       temperature: 0.7
     })
